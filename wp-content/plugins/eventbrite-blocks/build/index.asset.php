d45d4f93dfceb378f7bb479d516bc915a66ff49a
Jon Waldstein <jpwaldstein@gmail.com>
2020-04-08 17:59:03 -0400

    chore: rename eventbrite-blocks to blocks-for-eventbrite

--- wp-content/plugins/eventbrite-blocks/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4f2ee740b813279092bc1a385325c835');
--- wp-content/plugins/eventbrite-blocks/src/utilities/index.js
+++
@@ -1,7 +0,0 @@
-function getLocalizeData( ...props ) {
-	return props.map( ( prop ) => {
-		// eslint-disable-next-line no-undef
-		return eventbriteBlocks[ prop ];
-	} );
-}
-export { getLocalizeData };
